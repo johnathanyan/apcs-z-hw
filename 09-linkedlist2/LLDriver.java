b7aac5f9e703be5337adfd5508381c6d211ac131
johnathanyan <johnathanyan@gmail.com>
2014-03-26 06:28:53 -0400

    changed folder names

---
+++ 09-linkedlist2/LLDriver.java
@@ -0,0 +1,19 @@
+public class LLDriver {
+
+    public static void main(String[] args){
+	MyLinkedList L = new MyLinkedList();
+
+	L.add("A");
+	System.out.println(L);
+	L.add("B");
+	System.out.println(L);
+	L.add("C");
+	System.out.println(L);
+	L.add("D");
+	System.out.println(L);
+	L.add("E");
+	System.out.println(L);
+
+
+    }
+}
