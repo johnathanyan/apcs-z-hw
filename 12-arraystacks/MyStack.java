6e5e62c2653c154f523c9ccc268261b3501d85ac
johnathanyan <johnathanyan@gmail.com>
2014-04-07 08:35:14 -0400

    added wrong hw will update at home

--- 12-arraystacks/MyStack.java
+++ 12-arraystacks/MyStack.java
@@ -1,8 +1,9 @@
+//added wrong homework - will update at home
+
 public class MyStack {
     private int top;
-    private int start_length = 10;
     private int size;
-    private String[] stack = new String[start_length];
+    private String[] stack = new String[10];
     
     public MyStack() {
         top = stack.length - 1;
@@ -36,25 +37,4 @@ public class MyStack {
         return stack[top];
     }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public String toString() {
-        String s = "";
-        for (int i = top;i < stack.length;i++) {
-            s += stack[i] + ", ";
-        }
-        return s;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private void expand() {
-        String[] tmp = new String[stack.length * 2];
-        System.arraycopy(stack, top, tmp, top = tmp.length - size, size);
-        stack = tmp;
-    }
 }
