9a4b2f169726eea642cc801b3f6e1f85907b3937
johnathanyan <johnathanyan@gmail.com>
2014-04-07 19:20:02 -0400

    added correct hw 12

--- 12-arraystacks/MyStack.java
+++ 12-arraystacks/MyStack.java
@@ -1,40 +1,57 @@
-//added wrong homework - will update at home
-
 public class MyStack {
+
+	private String[] stack = new String[10];
     private int top;
-    private int size;
-    private String[] stack = new String[10];
-    
-    public MyStack() {
-        top = stack.length - 1;
+    private int numElts;
+
+    public MyStack(){
+		top = 0;
+		numElts = 0;
     }
 
-    public void push(String s) {
-        if (stack[top] == null) {
-            stack[top] = s;
-            size++;
-        }
-        else {
-            if (top == 0) {
-                expand();
-            }
-            stack[--top] = s;
-            size++;
-        }
-    }
-
+    public  void push(String s){
+	if (isEmpty()){
+		stack[0] = s;
+		numElts++;
+	}
+	else{
+		String[] newStack = new String[stack.length + 1];
+		for (int i = 0; i < stack.length; i++)
+			newStack[i] = stack[i];
+		numElts++;
+		newStack[numElts - 1] = s;
+		stack = newStack;
+		top++;
+	}
+	}
+	
     public String pop() {
-        String tmp = stack[top];
-        stack[top] = null;
-        if (top != stack.length - 1) {
-            top++;
-        }
-        size--;
-        return tmp;
+	String s;
+	s = stack[top];
+	stack[top] = "";
+	if (top != 0)
+		top = top - 1;
+	numElts = numElts - 1;
+	return s;
     }
 
     public String peek() {
-        return stack[top];
+	return stack[top];
     }
 
+    public boolean isEmpty() {
+	return numElts == 0;
+    }
+
+    public int getSize() {
+	return numElts;
+    }
+    
+    public String toString(){
+	String ret = "";
+    for (int x = top;x >= 0;x--){
+        ret += stack[x] + ", ";
+    }
+    return ret;
+    }
 }
