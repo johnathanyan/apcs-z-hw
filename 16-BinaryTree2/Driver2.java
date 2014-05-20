f46c8ea534efc7458023262f2058349e8800a09d
johnathanyan <johnathanyan@gmail.com>
2014-05-19 21:11:02 -0400

    Revert "fixed readme"
    This reverts commit 285bda50ba60c94125ee83a4be53792bb4573d21.

--- 16-BinaryTree2/Driver2.java
+++ 16-BinaryTree2/Driver2.java
@@ -5,7 +5,6 @@ public class Driver2 {
         test.insert(100);
         System.out.println(test.search(10));
         System.out.println(test.search(100));
-        System.out.println(test.piggySearch(100));
         System.out.println(test.search(75));
         System.out.println(test.search2(test.root,10));
         System.out.println(test.search2(test.root,100));
